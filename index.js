eb3ece0a03706f2d72a7b9362c935eda2058ecfd
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-01-30 14:55:14 +0200

    Add templating

--- index.js
+++ index.js
@@ -2,12 +2,34 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const nunjucks = require('nunjucks');
 
-app.get('/', (req, res) => {
-    console.log(path.resolve(__dirname, 'index.html'));
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
 });
 
+app.get('/', (req, res) => {
+  res.render('index.njk');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.njk');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact.njk');
+});
+
+app.get('/gallery', (req, res) => {
+  res.render('gallery.njk');
+});
+
+app.get('/values', (req, res) => {
+  res.render('values.njk');
+});
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
